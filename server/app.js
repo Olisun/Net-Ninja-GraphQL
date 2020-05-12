b72c343969d36124e6398d4d08042145801f5c4f
Oliver Sun <olisun@mac.com>
2020-05-12 12:03:52 -0700

    added practice data for books and authors. added root queries for each

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 // Setting up graphql as middleware to express. 
 app.use('/graphql', graphqlHTTP({
-
+  schema,
+  graphiql: true
 }));
 
 const PORT = 4000;
